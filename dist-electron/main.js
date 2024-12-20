79ef3571a4f95c08c36010789504989742315dc1
liubin <bh0bama@sina.com>
2024-12-20 17:59:40 +0800

    update ignore

--- dist-electron/main.js
+++
@@ -1,6 +0,0 @@
-import { app, BrowserWindow } from "electron";
-import path from "path";
-app.on("ready", () => {
-    const win = new BrowserWindow({});
-    win.loadFile(path.join(app.getAppPath(), "/dist-web/index.html"));
-});
