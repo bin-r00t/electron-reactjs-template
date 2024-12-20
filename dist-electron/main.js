779d0be94dcd458685ce267dc58e73a400947c6e
liubin <bh0bama@sina.com>
2024-12-20 14:27:06 +0800

    add: tsc for electron folder

---
+++ dist-electron/main.js
@@ -0,0 +1,6 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+app.on("ready", () => {
+    const win = new BrowserWindow({});
+    win.loadFile(path.join(app.getAppPath(), "/dist-web/index.html"));
+});
--- src/controller/main.ts
+++ src/controller/main.ts
@@ -1,6 +1,8 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
+type test = string;
+
 app.on("ready", () => {
   const win = new BrowserWindow({});
   win.loadFile(path.join(app.getAppPath(), "/dist-web/index.html"));
